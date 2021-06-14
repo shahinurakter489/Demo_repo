b3e14477d1efdbc71e6fd98191c98edfaeb1568f
mohsi <mohsi@LAPTOP-V9LR9B12.home>
2021-06-14 08:46:09 -0400

    this is my first commit

---
+++ src/test3.java
@@ -0,0 +1,14 @@
+
+public class test3 {
+
+	public static void main(String[] args) {
+		int x=0;
+		while(x<20) {
+			System.out.println(x);
+			x++;
+		}
+		System.out.println("End of loop");
+
+	}
+
+}
